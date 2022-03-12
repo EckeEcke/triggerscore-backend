680ed5e23a3f855ac37a36938b49964a267745d2
Christian Eckardt <eckardt.christian@web.de>
2022-03-12 15:19:34 +0100

    switching from 5 to 6 items

--- triggerscore-backend.js
+++ triggerscore-backend.js
@@ -46,7 +46,7 @@ app.get('/movie/:id', function(req,res) {
 })
 
 app.get('/recentratings', function(req,res) {
-  con.query(`SELECT * FROM triggerscore ORDER BY id DESC LIMIT 5`, function(err,result){
+  con.query(`SELECT * FROM triggerscore ORDER BY id DESC LIMIT 6`, function(err,result){
     if(err) throw err;
     else {
       res.send(result)
