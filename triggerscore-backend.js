3fc542889e1d8fca847dae7660f7cb5c29b1b8b6
Christian Eckardt <eckardt.christian@web.de>
2024-04-18 23:37:04 +0200

    cleanup

--- triggerscore-backend.js
+++ triggerscore-backend.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 3000;
-const http = require('http');
-const mysql = require('mysql');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+const http = require('http')
+const mysql = require('mysql')
 const con = mysql.createConnection({
   host: "sql11.freesqldatabase.com",
   database: "sql11700093",
   user: "sql11700093",
   password: process.env.DATABASE_PASSWORD,
   port: 3306
-});
+})
 
-const sql = `SELECT * FROM triggerscore ORDER BY id ASC`;
+const sql = `SELECT * FROM triggerscore ORDER BY id ASC`
 
 con.connect(function(err) {
-  if(err) throw err;
-  console.log("Connected!");
+  if(err) throw err
+  console.log("Connected!")
   con.query(sql, function (err, result) {
-     if (err) throw err;
-  });
-});
+     if (err) throw err
+  })
+})
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.get('/', function (req, res) {
   con.query(sql, function(err,result){
-    if (err) throw err;
+    if (err) throw err
     else {
-      let calculatedScores = calculateScores(result)
-      res.send(calculatedScores);
+      const calculatedScores = calculateScores(result)
+      res.send(calculatedScores)
     }})
-}).listen(port);
+}).listen(port)
 
 app.get('/movie/:id', function(req,res) {
   con.query(`SELECT * FROM triggerscore WHERE movie_id = ${req.params.id}`, function(err,result){
-    if(err) throw err;
+    if(err) throw err
     else {
       let calculatedScore = calculateScores(result)
       res.send(calculatedScore)
@@ -46,7 +46,7 @@ app.get('/movie/:id', function(req,res) {
 
 app.get('/recentratings', function(req,res) {
   con.query(`SELECT * FROM triggerscore ORDER BY id DESC LIMIT 6`, function(err,result){
-    if(err) throw err;
+    if(err) throw err
     else {
       res.send(result)
     }
@@ -55,7 +55,7 @@ app.get('/recentratings', function(req,res) {
 
 app.get('/recentcomments', function(req,res) {
   con.query(`SELECT * FROM triggerscore WHERE comment IS NOT NULL AND comment != '' ORDER BY id DESC LIMIT 8`, function(err,result){
-    if(err) throw err;
+    if(err) throw err
     else {
       res.send(result)
     }
@@ -64,54 +64,53 @@ app.get('/recentcomments', function(req,res) {
 
 app.get('/top10-sexism', function(req,res){
   con.query(sql, function(err,result){
-    if (err) throw err;
+    if (err) throw err
     else {
-      let calculatedScores = calculateScores(result)
-      let top10 = calculatedScores.sort((a,b)=> {return b.rating_sexism - a.rating_sexism})
+      const calculatedScores = calculateScores(result)
+      const top10 = calculatedScores.sort((a,b)=> {return b.rating_sexism - a.rating_sexism})
       res.send(top10.slice(0,10));
     }})
 })
 
 app.get('/top10-racism', function(req,res){
   con.query(sql, function(err,result){
-    if (err) throw err;
+    if (err) throw err
     else {
-      let calculatedScores = calculateScores(result)
-      let top10 = calculatedScores.sort((a,b)=> {return b.rating_racism - a.rating_racism})
-      res.send(top10.slice(0,10));
+      const calculatedScores = calculateScores(result)
+      const top10 = calculatedScores.sort((a,b)=> {return b.rating_racism - a.rating_racism})
+      res.send(top10.slice(0,10))
     }})
 })
 
 app.get('/top10-others', function(req,res){
   con.query(sql, function(err,result){
-    if (err) throw err;
+    if (err) throw err
     else {
-      let calculatedScores = calculateScores(result)
-      let top10 = calculatedScores.sort((a,b)=> {return b.rating_others - a.rating_others})
-      res.send(top10.slice(0,10));
+      const calculatedScores = calculateScores(result)
+      const top10 = calculatedScores.sort((a,b)=> {return b.rating_others - a.rating_others})
+      res.send(top10.slice(0,10))
     }})
 })
 
 app.get('/top10-cringe', function(req,res){
   con.query(sql, function(err,result){
-    if (err) throw err;
+    if (err) throw err
     else {
-      let calculatedScores = calculateScores(result)
-      let top10 = calculatedScores.sort((a,b)=> {return b.rating_cringe - a.rating_cringe})
-      res.send(top10.slice(0,10));
+      const calculatedScores = calculateScores(result)
+      const top10 = calculatedScores.sort((a,b)=> {return b.rating_cringe - a.rating_cringe})
+      res.send(top10.slice(0,10))
     }})
 })
 
 app.get('/stats', function(req,res){
   con.query(sql, function(err,result){
-  if (err) throw err;
+  if (err) throw err
     else {
-      let calculatedScores = calculateScores(result)
-      let amountComments =  countComments(result)
-      let amountLikes = countLikesAndDislikes(result)
-      console.log(amountComments)
-      let totalRatings = result.length
-      let totalMovies = calculatedScores.length
+      const calculatedScores = calculateScores(result)
+      const amountComments =  countComments(result)
+      const amountLikes = countLikesAndDislikes(result)
+      const totalRatings = result.length
+      const totalMovies = calculatedScores.length
       let allScoresTotal = 0
       let allScoresSexism = 0
       let allScoresRacism = 0
@@ -122,12 +121,12 @@ app.get('/stats', function(req,res){
       calculatedScores.forEach(score=>{allScoresRacism = allScoresRacism + score.rating_racism})
       calculatedScores.forEach(score=>{allScoresOthers = allScoresOthers + score.rating_others})
       calculatedScores.forEach(score=>{allScoresCringe = allScoresCringe + score.rating_cringe})
-      let averageScoreTotal = Math.floor(allScoresTotal / totalMovies * 10) / 10
-      let averageScoreSexism = Math.floor(allScoresSexism/ totalMovies * 10) / 10
-      let averageScoreRacism = Math.floor(allScoresRacism / totalMovies * 10) / 10
-      let averageScoreOthers = Math.floor(allScoresOthers / totalMovies * 10) / 10
-      let averageScoreCringe = Math.floor(allScoresCringe / totalMovies * 10) / 10
-      let stats = {
+      const averageScoreTotal = Math.floor(allScoresTotal / totalMovies * 10) / 10
+      const averageScoreSexism = Math.floor(allScoresSexism/ totalMovies * 10) / 10
+      const averageScoreRacism = Math.floor(allScoresRacism / totalMovies * 10) / 10
+      const averageScoreOthers = Math.floor(allScoresOthers / totalMovies * 10) / 10
+      const averageScoreCringe = Math.floor(allScoresCringe / totalMovies * 10) / 10
+      const stats = {
                     "totalRatings":totalRatings,
                     "averageScoreTotal":averageScoreTotal,
                     "averageScoreSexism":averageScoreSexism,
@@ -139,7 +138,7 @@ app.get('/stats', function(req,res){
                     "amountLikes": amountLikes.likes,
                     "amountDislikes": amountLikes.dislikes
                   }
-      res.send(stats);
+      res.send(stats)
     }})
 })
 
@@ -153,9 +152,8 @@ function calculateScores(data){
     if(entry.comment != null && entry.comment.length > 3){
       comments += 1
     }
-    let index = scores.map(score => score.movie_id).indexOf(entry.movie_id)
-    // let entryTotal = (entry.rating_sexism + entry.rating_racism + entry.rating_others) / 3  ORIGINAL CALCULATION
-    let entryTotal = calculateTotal(entry) // NEW CALCULATION
+    const index = scores.map(score => score.movie_id).indexOf(entry.movie_id)
+    const entryTotal = calculateTotal(entry)
     if(index == -1) {
       entry.rating_total = entryTotal
       entry.ratings = 1
@@ -170,14 +168,12 @@ function calculateScores(data){
       }
       scores.push(entry)
     } else {
-      console.log(scores[index].likes)
       scores[index].ratings += 1
       scores[index].rating_sexism += entry.rating_sexism
       scores[index].rating_racism += entry.rating_racism
       scores[index].rating_others += entry.rating_others
       scores[index].rating_cringe += entry.rating_cringe
       scores[index].rating_total += entryTotal
-      console.log(entry.disliked)
       if(entry.liked == 1){
         scores[index].likes += 1
       }
@@ -213,7 +209,6 @@ function calculateTotal(score){
   const sexism = score.rating_sexism
   const others = score.rating_others
   const highest = Math.max(racism,sexism,others)
-  // console.log(highest,((score.rating_sexism + score.rating_racism + score.rating_others) / 3),((racism + sexism + others + highest*5) / 8  ))
   return (racism + sexism + others + highest*5) / 8  
 }
 
@@ -236,15 +231,13 @@ function countLikesAndDislikes(data){
 
 
 app.post('/post', function(request,response){
-  //const postSQL = 'INSERT INTO highscores (Player, Score) VALUES ([request.body.Player],[request.body.Score])';
-  console.log(request.body);
   if(request.body.movieID == undefined) {
     console.log("no movie defined in request body.............")
   }
   if(request.body.movieID != undefined){
     con.query('INSERT INTO triggerscore (movie_id,rating_sexism, rating_racism, rating_others, rating_cringe, comment, liked, disliked) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[request.body.movieID,request.body.sexism,request.body.racism, request.body.others, request.body.cringe, request.body.comment, request.body.like, request.body.dislike], function (err) {
-      if(err) throw err;
-      else {response.send("Received request")};
+      if(err) throw err
+      else {response.send("Received request")}
   })
   }
 })
