bc973fac6f71fb7c7c579e78b4ecbc3fa7209a2c
Christian Eckardt <eckardt.christian@web.de>
2024-08-29 23:38:38 +0200

    migrate to mongodb

--- triggerscore-backend.js
+++ triggerscore-backend.js
@@ -1,147 +1,33 @@
 const express = require('express')
+const { MongoClient } = require("mongodb")
 const cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 3000
-const http = require('http')
-const mysql = require('mysql')
-const con = mysql.createConnection({
-  host: "sql11.freesqldatabase.com",
-  database: "sql11700093",
-  user: "sql11700093",
-  password: process.env.DATABASE_PASSWORD,
-  port: 3306
-})
-
-const sql = `SELECT * FROM triggerscore ORDER BY id ASC`
-
-con.connect(function(err) {
-  if(err) throw err
-  console.log("Connected!")
-  con.query(sql, function (err, result) {
-     if (err) throw err
-  })
-})
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', function (req, res) {
-  con.query(sql, function(err,result){
-    if (err) throw err
-    else {
-      const calculatedScores = calculateScores(result)
-      res.send(calculatedScores)
-    }})
-}).listen(port)
-
-app.get('/movie/:id', function(req,res) {
-  con.query(`SELECT * FROM triggerscore WHERE movie_id = ${req.params.id}`, function(err,result){
-    if(err) throw err
-    else {
-      let calculatedScore = calculateScores(result)
-      res.send(calculatedScore)
-    }
-  })
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
 })
 
-app.get('/recentratings', function(req,res) {
-  con.query(`SELECT * FROM triggerscore ORDER BY id DESC LIMIT 6`, function(err,result){
-    if(err) throw err
-    else {
-      res.send(result)
-    }
-  })
-})
+let database
+const uri = "mongodb+srv://ceckardt254:CjDU44xSFTJ1g5JG@cluster0.sen83.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri)
 
-app.get('/recentcomments', function(req,res) {
-  con.query(`SELECT * FROM triggerscore WHERE comment IS NOT NULL AND comment != '' ORDER BY id DESC LIMIT 8`, function(err,result){
-    if(err) throw err
-    else {
-      res.send(result)
-    }
-  })
-})
+async function run() {
+  try {
+    database = client.db('triggerscore')
+    const scores = database.collection('scores')
+    const query = { "id": "3" }
+  }
+  catch {
+    throw error
+  }
+}
 
-app.get('/top10-sexism', function(req,res){
-  con.query(sql, function(err,result){
-    if (err) throw err
-    else {
-      const calculatedScores = calculateScores(result)
-      const top10 = calculatedScores.sort((a,b)=> {return b.rating_sexism - a.rating_sexism})
-      res.send(top10.slice(0,10));
-    }})
-})
-
-app.get('/top10-racism', function(req,res){
-  con.query(sql, function(err,result){
-    if (err) throw err
-    else {
-      const calculatedScores = calculateScores(result)
-      const top10 = calculatedScores.sort((a,b)=> {return b.rating_racism - a.rating_racism})
-      res.send(top10.slice(0,10))
-    }})
-})
-
-app.get('/top10-others', function(req,res){
-  con.query(sql, function(err,result){
-    if (err) throw err
-    else {
-      const calculatedScores = calculateScores(result)
-      const top10 = calculatedScores.sort((a,b)=> {return b.rating_others - a.rating_others})
-      res.send(top10.slice(0,10))
-    }})
-})
-
-app.get('/top10-cringe', function(req,res){
-  con.query(sql, function(err,result){
-    if (err) throw err
-    else {
-      const calculatedScores = calculateScores(result)
-      const top10 = calculatedScores.sort((a,b)=> {return b.rating_cringe - a.rating_cringe})
-      res.send(top10.slice(0,10))
-    }})
-})
-
-app.get('/stats', function(req,res){
-  con.query(sql, function(err,result){
-  if (err) throw err
-    else {
-      const calculatedScores = calculateScores(result)
-      const amountComments =  countComments(result)
-      const amountLikes = countLikesAndDislikes(result)
-      const totalRatings = result.length
-      const totalMovies = calculatedScores.length
-      let allScoresTotal = 0
-      let allScoresSexism = 0
-      let allScoresRacism = 0
-      let allScoresOthers = 0
-      let allScoresCringe = 0
-      calculatedScores.forEach(score=>{allScoresTotal = allScoresTotal + score.rating_total})
-      calculatedScores.forEach(score=>{allScoresSexism = allScoresSexism + score.rating_sexism})
-      calculatedScores.forEach(score=>{allScoresRacism = allScoresRacism + score.rating_racism})
-      calculatedScores.forEach(score=>{allScoresOthers = allScoresOthers + score.rating_others})
-      calculatedScores.forEach(score=>{allScoresCringe = allScoresCringe + score.rating_cringe})
-      const averageScoreTotal = Math.floor(allScoresTotal / totalMovies * 10) / 10
-      const averageScoreSexism = Math.floor(allScoresSexism/ totalMovies * 10) / 10
-      const averageScoreRacism = Math.floor(allScoresRacism / totalMovies * 10) / 10
-      const averageScoreOthers = Math.floor(allScoresOthers / totalMovies * 10) / 10
-      const averageScoreCringe = Math.floor(allScoresCringe / totalMovies * 10) / 10
-      const stats = {
-                    "totalRatings":totalRatings,
-                    "averageScoreTotal":averageScoreTotal,
-                    "averageScoreSexism":averageScoreSexism,
-                    "averageScoreRacism":averageScoreRacism,
-                    "averageScoreOthers":averageScoreOthers,
-                    "averageScoreCringe":averageScoreCringe,
-                    "amountMovies":totalMovies,
-                    "amountComments": amountComments,
-                    "amountLikes": amountLikes.likes,
-                    "amountDislikes": amountLikes.dislikes
-                  }
-      res.send(stats)
-    }})
-})
-
+run().catch(console.dir)
 
 function calculateScores(data){
   let scores = []
@@ -227,17 +113,168 @@ function countLikesAndDislikes(data){
   })
   return likes
 }
-
-
-
-app.post('/post', function(request,response){
-  if(request.body.movieID == undefined) {
-    console.log("no movie defined in request body.............")
+app.get('/', async (req, res) => {
+  try {
+    const scores = await database.collection('scores').find().toArray()
+    res.json(scores)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
   }
-  if(request.body.movieID != undefined){
-    con.query('INSERT INTO triggerscore (movie_id,rating_sexism, rating_racism, rating_others, rating_cringe, comment, liked, disliked) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[request.body.movieID,request.body.sexism,request.body.racism, request.body.others, request.body.cringe, request.body.comment, request.body.like, request.body.dislike], function (err) {
-      if(err) throw err
-      else {response.send("Received request")}
-  })
+})
+
+app.get('/movie/:id', async (req,res) => {
+  try {
+    const ratings = await database.collection('scores').find({ "movie_id": req.params.id }).toArray()
+    res.json(calculateScores(ratings))
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.get('/recentratings', async (req,res) => {
+  try {
+    const ratings = await database.collection('scores').find().sort( { id: -1 }).limit(6).toArray()
+    res.json(ratings)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.get('/recentcomments', async (req,res) => {
+  try {
+    const ratings = await database.collection('scores')
+    .find(
+      { 
+        comment: { $ne: null, $ne: "" } 
+      }
+    ).sort(
+      { id: -1 }
+    ).limit(8)
+    .toArray()
+    res.json(ratings)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.get('/top10-sexism', async (req,res) => {
+    try {
+      const ratings = await database.collection('scores')
+      .find()
+      .sort({ rating_sexism: -1 })
+      .limit(10)
+      .toArray()
+      res.json(ratings)
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+})
+
+app.get('/top10-racism', async (req,res) => {
+    try {
+      const ratings = await database.collection('scores')
+      .find()
+      .sort({ rating_racism: -1 })
+      .limit(10)
+      .toArray()
+      res.json(ratings)
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+})
+
+app.get('/top10-others', async (req,res) => {
+  try {
+    const ratings = await database.collection('scores')
+      .find()
+      .sort({ rating_others: -1 })
+      .limit(10)
+      .toArray()
+      res.json(ratings)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.get('/top10-cringe', async (req,res) => {
+  try {
+    const ratings = await database.collection('scores')
+      .find()
+      .sort({ rating_cringe: -1 })
+      .limit(10)
+      .toArray()
+      res.json(ratings)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.get('/stats', async (req,res) => {
+  try {
+    const ratings = await database.collection('scores')
+      .find()
+      .sort({ rating_cringe: -1 })
+      .limit(10)
+      .toArray()
+      const calculatedScores = calculateScores(ratings)
+      const amountComments =  countComments(ratings)
+      const amountLikes = countLikesAndDislikes(ratings)
+      const totalRatings = ratings.length
+      const totalMovies = calculatedScores.length
+      let allScoresTotal = 0
+      let allScoresSexism = 0
+      let allScoresRacism = 0
+      let allScoresOthers = 0
+      let allScoresCringe = 0
+      calculatedScores.forEach(score=>{allScoresTotal = allScoresTotal + score.rating_total})
+      calculatedScores.forEach(score=>{allScoresSexism = allScoresSexism + score.rating_sexism})
+      calculatedScores.forEach(score=>{allScoresRacism = allScoresRacism + score.rating_racism})
+      calculatedScores.forEach(score=>{allScoresOthers = allScoresOthers + score.rating_others})
+      calculatedScores.forEach(score=>{allScoresCringe = allScoresCringe + score.rating_cringe})
+      const averageScoreTotal = Math.floor(allScoresTotal / totalMovies * 10) / 10
+      const averageScoreSexism = Math.floor(allScoresSexism/ totalMovies * 10) / 10
+      const averageScoreRacism = Math.floor(allScoresRacism / totalMovies * 10) / 10
+      const averageScoreOthers = Math.floor(allScoresOthers / totalMovies * 10) / 10
+      const averageScoreCringe = Math.floor(allScoresCringe / totalMovies * 10) / 10
+      const stats = {
+                    "totalRatings":totalRatings,
+                    "averageScoreTotal":averageScoreTotal,
+                    "averageScoreSexism":averageScoreSexism,
+                    "averageScoreRacism":averageScoreRacism,
+                    "averageScoreOthers":averageScoreOthers,
+                    "averageScoreCringe":averageScoreCringe,
+                    "amountMovies":totalMovies,
+                    "amountComments": amountComments,
+                    "amountLikes": amountLikes.likes,
+                    "amountDislikes": amountLikes.dislikes
+                  }
+      res.send(stats)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.post('/post', async (request,response) => {
+  if (request.body.movieID === undefined) {
+    console.log("No movie defined in request body...")
+    response.status(400).send("Movie ID is required")
+    return
+  }
+
+  try {
+    const result = await database.collection('triggerscore').insertOne({
+      movie_id: request.body.movieID,
+      rating_sexism: request.body.sexism,
+      rating_racism: request.body.racism,
+      rating_others: request.body.others,
+      rating_cringe: request.body.cringe,
+      comment: request.body.comment,
+      liked: request.body.like,
+      disliked: request.body.dislike
+    })
+    response.send("Received request")
+  } catch (err) {
+    console.error(err);
+    response.status(500).send("Error inserting data")
   }
 })
